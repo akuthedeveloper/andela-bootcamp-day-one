d2e71cde3e51e7bf25d3ddf93a2e7efda5692d00
akuthedeveloper <kevakuviza@gmail.com>
2016-12-04 22:03:03 +0300

    Add files via upload

--- Andela-day-one/fizzbuzz.py
+++ Andela-day-one/fizzbuzz.py
@@ -1,12 +1,16 @@
 def fizz_buzz(number):
+    # checks if the number is divisible by both 3 and 5 and returns FizzBuzz
     if number%15 == 0:
         return "FizzBuzz"
+    # checks if the number is divisible by 3
     elif number%3 == 0:
         return "Fizz"
+    # checks if the number is divisible by 5
     elif number%5 == 0:
         return "Buzz"
     else:
         return number
+        # print statement for all the condition
 print fizz_buzz(30)
 print fizz_buzz(9)
 print fizz_buzz(5)
